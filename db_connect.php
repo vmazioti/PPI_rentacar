02687b4951acd9aeeaa796671faf603c3e2422ee
vmazioti <smazioti@gmail.com>
2022-06-19 19:19:34 +0300

    html and css

---
+++ db_connect.php
@@ -0,0 +1,14 @@
+<?php  
+
+$host_name = "localhost";
+$user_name = "root";
+$password = "";
+$db_name = "carcatalogdb";
+
+$conn = mysqli_connect($host_name, $user_name, $password, $db_name);
+
+if (!$conn) {
+	echo "Failure: Could not connect to database!";
+	exit();
+}
+?>
