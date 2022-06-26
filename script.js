7e61004a39ff7a60bb4f6adc4463d88732e07992
Vasiliki Mazioti <smazioti@gmail.com>
2022-06-26 12:39:09 +0300

    add chart.js example

---
+++ script.js
@@ -0,0 +1,54 @@
+// $(document).ready(selectCategory() {
+//   $("select").trigger("change");
+// });
+function selectCategory() {
+  var a = document.getElementById("categorySelection").value;
+  // $.ajax({
+  //   url: "",
+  //   method: "POST",
+  //   data: {
+  //     id: a,
+  //   },
+  //   success: function (data) {},
+  // });
+
+  document.getElementById("car_total").innerHTML = "5 + 6";
+}
+
+const ctx = document.getElementById("myChart").getContext("2d");
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
